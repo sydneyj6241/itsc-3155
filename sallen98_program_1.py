26d91fddcddc117c8ea4716c4757baf38da71d18
Sydney <sallen98@uncc.edu>
2022-09-07 19:17:51 -0400

    Final Program 1

--- sallen98_program_1.py
+++ sallen98_program_1.py
@@ -1,9 +1,13 @@
 #Program 1
+
 def string_both_ends(str):
     if len(str) < 2:
-        return ''
-    return str[0:2] + str[-2]
+        print("Your string is too short!")
 
-string = input input("Enter your String:")
+    else:
+        return str[0:2] + str[-2:]
+
+
+string = input("Enter your String:")
 stringToReturn = string_both_ends(string)
 print(stringToReturn)
